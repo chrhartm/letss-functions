bc3207c64a43c9caab7cd86263552980e1e761ab
Christoph Hartmann <c.hartmann@carepayinternational.com>
2021-09-26 19:35:10 +0200

    MVP matching function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,32 +5,90 @@ admin.initializeApp();
 
 exports.generateMatches = functions.https.onRequest(
     async (request, response) => {
-    // Grab the text parameter.
+      const N = 30;
       const userid = String(request.query.userid);
-      // Push the new message into Firestore using the Firebase Admin SDK.
+      const db = admin.firestore();
 
-      // TODO select only relevant fields
-      const userinfo = (await admin.firestore().collection("users")
+      // TODO select only relevant fields (not pic)
+      const userinfo = (await db.collection("users")
           .doc(userid).get()).data();
-      if ( userinfo == null ) {
-        response.status(500).send("Couldn't find user i");
+      if (userinfo == null) {
+        response.status(500).send("Couldn't find user id");
+        return;
       }
-      let list: []
+      const lastSearch = userinfo!.lastSearch;
+      if ((admin.firestore.Timestamp.now().toMillis() -
+          lastSearch.toMillis()) < (1000 * 60 * 60)) {
+        response.status(429).send("Already requested within last hour");
+        return;
+      }
+      const activities = new Set();
       for (const category of userinfo!.interests) {
-        console.log(category);
-        // For each category, get the latest XX matches since last search if category timestamp < time of last search, otherwise get all last XX regarless of timestamp
         // TODO filter by location
-        // TODO filter on status active
-        console.log((await (admin.firestore().collection('activities').where("categories", "array-contains", category).orderBy("timestamp")).select("categories").get())))
+        await db.collection("activities")
+            .where("status", "==", "ACTIVE")
+            .where("timestamp", ">", lastSearch)
+            .where("categories", "array-contains", category)
+            .orderBy("timestamp", "desc")
+            .limit(N)
+            .select("user")
+            .get()
+            .then((querySnapshot) => {
+              querySnapshot.forEach((doc) => {
+                if (doc.data()["user"] != userid) {
+                  activities.add(doc.id);
+                }
+              });
+            })
+            .catch(function(error) {
+              console.log("Error getting documents: ", error);
+              response.status(500).send("Unknown error");
+              return;
+            });
       }
-      // TODO add a few that don't match interests for good measure
-      // TODO get all previous proposals younger than oldest timestamp
-      // TODO substract previous proposals
-      // TODO shuffle
-      // TODO write new proposals
-      // const writeResult = await admin.firestore().collection('messages')
-      // .add({original: original});
-      // Send back a message that we've successfully written the message
-      response.json({result: `Received: ${userinfo}`});
+      // add some without interest filter
+      // in case interests are too specific
+      await db.collection("activities")
+          .where("status", "==", "ACTIVE")
+          .where("timestamp", ">", lastSearch)
+          .orderBy("timestamp", "desc")
+          .limit(N)
+          .select("user")
+          .get()
+          .then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+              if (doc.data()["user"] != userid) {
+                activities.add(doc.id);
+              }
+            });
+          })
+          .catch(function(error) {
+            console.log("Error getting documents: ", error);
+            response.status(500).send("Unknown error");
+            return;
+          });
+      console.log(activities);
+
+      const now = admin.firestore.Timestamp.now();
+      db.collection("users").doc(userid)
+          .update({lastSearch: now});
+      console.log("after user update");
+
+      if (activities.size == 0) {
+        response.status(204).send("No new activities available");
+        return;
+      }
+
+      console.log("before batch");
+      const batch = db.batch();
+      activities.forEach((doc) => {
+        const data = {activity: doc, user: userid, status: "NEW",
+          timestamp: now};
+        batch.set(db.collection("matches").doc(doc+"_"+userid), data);
+      });
+      batch.commit();
+      console.log("after batch");
+
+      response.status(200).send("Generated new matches");
     }
 );
