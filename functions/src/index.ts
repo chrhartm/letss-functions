b59796fed6783a21aa6df079c8efad7a4d2873cf
Christoph Hartmann <c.hartmann@carepayinternational.com>
2021-10-24 21:20:36 +0200

    added bootstrapping script

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,12 +1,14 @@
-import admin = require("firebase-admin");
+import firebaseAdmin = require("firebase-admin");
 import activity = require("./activity");
 import messaging = require("./messaging");
 import user = require("./user");
 import categories = require("./categories");
+import admin = require("./admin");
 
-admin.initializeApp();
+firebaseAdmin.initializeApp();
 
 exports.user = user;
 exports.activity = activity;
 exports.messaging = messaging;
 exports.categories = categories;
+exports.admin = admin;
