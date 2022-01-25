95983ecdc9e084d03c0ccd71176a957dd8f236ef
Christoph Hartmann <christophartmann@gmail.com>
2022-01-25 22:22:09 +0300

    Added email on like

--- functions/src/messaging.ts
+++ functions/src/messaging.ts
@@ -1,18 +1,22 @@
 import functions = require("firebase-functions");
 import admin = require("firebase-admin");
+import sendGridClient = require("@sendgrid/mail");
+
 
 exports.pushOnLike = functions.region("europe-west1").firestore
     .document("/activities/{activityId}/likes/{likeId}")
     .onCreate((snap, context) => {
       const db = admin.firestore();
       const like = snap.data();
+      // Get data on sender
       return db.collection("persons").doc(snap.id)
           .get().then((senderDoc) => {
             if (senderDoc.exists == false) {
-              console.log("Couldn't find user: " + snap.id);
+              console.log("Couldn't find person: " + snap.id);
               return null;
             }
             const senderP = senderDoc.data()!;
+            // Get data on activity
             return db.collection("activities").doc(context.params.activityId)
                 .get().then((activity) => {
                   if (activity.exists == false) {
@@ -20,6 +24,7 @@ exports.pushOnLike = functions.region("europe-west1").firestore
                         context.params.activityId);
                     return null;
                   }
+                  // Get data on receiver
                   return db.collection("users").doc(activity.data()!.user)
                       .get().then((receiverDoc) => {
                         if (receiverDoc.exists == false) {
@@ -28,6 +33,12 @@ exports.pushOnLike = functions.region("europe-west1").firestore
                           return null;
                         }
                         const receiverU = receiverDoc.data()!;
+                        const now = admin.firestore.Timestamp.now().seconds;
+                        const daysUnopened = 60*60*24*3 + now;
+                        const daysOpened = 60*60*24 + now;
+                        const lastEmail = receiverU.lastEmail==null?null:
+                            receiverU.lastEmail.seconds;
+                        const lastOnline = receiverU.lastOnline.seconds;
                         const payload = {
                           notification: {
                             title: senderP.name,
@@ -38,9 +49,52 @@ exports.pushOnLike = functions.region("europe-west1").firestore
                         console.log("Sending message to " +
                             activity.data()!.user +
                             ": " + payload);
+                        // Send push notification
                         return admin.messaging()
                             .sendToDevice(receiverU.token.token, payload)
-                            .then((response) => console.log(response.results));
+                            .then((response) => {
+                              console.log("Successfully sent message:",
+                                  response);
+                              // Check if email should be sent
+                              // Send if not sent before or
+                              // last was sent > 3 days ago or
+                              // last was sent > 1 day ago and opened app
+                              if (lastEmail != null &&
+                                (!(lastOnline > lastEmail &&
+                                  lastEmail > daysOpened) &&
+                                (lastEmail < daysUnopened))) {
+                                return null;
+                              }
+                              // Get user email
+                              return admin.auth().getUser(activity.data()!.user)
+                                  .then((userRecord) => {
+                                    // Send email
+                                    return sendEmail(
+                                        "d-93478b18f7ee4935b554dea49749663e",
+                                        "noreply@letss.app",
+                                        userRecord.email!,
+                                        {name: senderP.name as string,
+                                          activity:
+                                            activity.data()!.name as string,
+                                          link: "https://letss.page.link/myactivities",
+                                        })
+                                        .then((response) => {
+                                          console.log(
+                                              "Successfully sent email:",
+                                              response);
+                                          // Update last email timestamp
+                                          return db.collection("users")
+                                              .doc(activity.data()!.user)
+                                              .update({lastEmail:
+                                                admin.firestore.Timestamp
+                                                    .fromMillis(now)})
+                                              .then((response) => {
+                                                console.log("Updated user",
+                                                    response);
+                                              });
+                                        });
+                                  });
+                            });
                       });
                 });
           });
@@ -88,4 +142,28 @@ exports.pushOnMessage = functions.region("europe-west1").firestore
                 });
           });
     });
+
+// requires firebase functions:config:set sendgrid.key="KEY"
+/**
+   * Send an email
+   * @param {string} templateId - sendGrid template ID
+   * @param {string} fromAddress - sender address
+   * @param {string} toAddress - address to send to
+   * @param {any} data - data to be sent as json
+   * @return {function} - Some function
+   */
+async function sendEmail(templateId: string,
+    fromAddress: string,
+    toAddress: string,
+    data: any) {
+  sendGridClient.setApiKey(functions.config().sendgrid.key);
+
+  const mailData = {
+    to: toAddress,
+    from: fromAddress,
+    templateId: templateId,
+    dynamic_template_data: data,
+  };
+  return sendGridClient.send(mailData);
+}
 
